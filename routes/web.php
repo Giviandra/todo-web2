d1e42a20dc7241090e29cdd6817508043c11210f
EgiViandra <andiegiviandra@gmail.com>
2026-05-04 20:06:07 +0800

    implementasi fungsi POST create task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController; // Tambahkan ini agar Laravel mengenali controller-mu
 
+// Route untuk menampilkan halaman form (sementara, nanti akan disempurnakan partnermu)
 Route::get('/', function () {
-    return view('welcome');
+    return view('todo');
 });
+
+// Route untuk menangani proses simpan data dari form [POST]
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
